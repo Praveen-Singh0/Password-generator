d2118788b299346aabebff7ddc0aa4a82cd2cac1
Praveen Singh <108598845+Praveen-Singh0@users.noreply.github.com>
2023-04-17 10:04:17 +0530

    initial Commit 1

--- script.js
+++ script.js
@@ -1,86 +1,82 @@
-const apikey = "79ee7cd3ec794b0b50649348febfd65b";
+const passwordEl = document.getElementById("password");
+const copyEl = document.getElementById("copy");
 
-const a = document.getElementById("h1");
-const b = document.getElementById("temp");
-const c = document.getElementById("immg");
-const main = document.getElementById("main");
-const form = document.getElementById("search");
-const input = document.getElementById("input");
-
-
-const date = new Date();
-let day = date.getDate();
-let month = date.getMonth();
-let year = date.getFullYear();
+const lengthEL = document.getElementById("length");
+const upperEl = document.getElementById("upper");
+const LowerEl = document.getElementById("lower");
+const numbers = document.getElementById("number");
+const symbols = document.getElementById("symbol");
+const generate = document.getElementById("generate");
 
 
+const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lower = "abcdefghijklmnopqrstuvwxyz";
+const number = "0123456789";
+const symbol = "~!@#$%^&*()_+}{|:";
 
 
-
-const API = (city) =>
-    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}`;
-
-async function getWeatherByLocation(city) {
-    const resp = await fetch(API(city), { origin: "cors" });
-    const respData = await resp.json();
-  
-    console.log(respData);
-  
-    getData(respData);
+function getUppercase(){
+    return upper[Math.floor(Math.random() * upper.length)]
 }
-function getData(data){
-  
-  var tt = data.main.temp - 273.15;
-  var td = data.main.temp_min - 273.15;
-  
-  // console.log(tt.toFixed(2));
-  
-  const we = document.createElement("div");  
-  we.innerHTML = `<div class = "head">
-    <h4>${day}-${month}-${year}</h4>
-    <h1 id="h1">${data.name}</h1>
-     </div>
-    <div class="display" id="immg" >
-      <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt="">
-        <div class="temp">
-        <h2 id="temp"> temp : ${tt.toFixed(2)} C </h2>
-        <h2 class="h">Min : ${td.toFixed(2)} C</h2>
-        <div class="tempchild">
-          <span id="dis">Wind : ${data.wind.speed}</span>
-          <span>${data.weather[0].description} Today</span>
-          </div>
-      </div>
-    </div>
-  <div class="days">
-    <div>Sunday
-    <div> <i class="fa-solid fa-cloud"></i> </div>
-    </div>
-    <div>monday
-    <div> <i class="fa-solid fa-bolt"></i> </div>
-    </div>
-    <div>tuesday
-    <div> <i class="fa-solid fa-sun"></i> </div></div>
-    <div>wednsday 
-    <div> <i class="fa-solid fa-wind"></i> </div></div>
-    <div>thrusday
-    <div> <i class="fa-solid fa-sun"></i> </div>
-    </div>
-    <div>friday 
-    <div> <i class="fa-solid fa-sun"></i> </div> 
-    </div>
-    </div>`;
- 
-  main.appendChild(we);
-  
+function getLowercase(){
+    return lower[Math.floor(Math.random() * lower.length)]
+}
+function getNumbers(){
+    return number[Math.floor(Math.random() * number.length)]
+}
+function getSymbols(){
+    return symbol[Math.floor(Math.random() * symbol.length)]
 }
 
-form.addEventListener('submit', (e)=>{
-  e.preventDefault();
-  
-  const city = input.value;
-  if(city){
+
+
+
+function generatePassword(){
+    const len = lengthEL.value;
+    let password = " ";
+    for(i=0; i<len; i++){
+        const x = generateP();
+        password += x;     
+    }
+    passwordEl.innerHTML = password;
     
-    getWeatherByLocation(city);
-  }
-  input.value = "";
-});
+}
+function generateP(){
+const ps = [];
+    if(upperEl.checked){
+        ps.push(getUppercase());
+    }
+    if(LowerEl.checked){
+        ps.push(getLowercase());
+    }
+    if(numbers.checked){
+        ps.push(getNumbers());
+    }
+    if(symbols.checked){
+        ps.push(getSymbols());
+    }
+    // console.log(ps);
+
+    return ps[Math.floor(Math.random() * ps.length)]
+
+}
+
+generate.addEventListener('click', generatePassword);
+
+copyEl.addEventListener('click', (e)=>{
+    const textarea =document.createElement("textarea");
+    const password = passwordEl.innerText;
+
+    if(!password){
+        return;
+    }
+
+    textarea.value = password;
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand("copy");
+    textarea.remove();
+    alert("Password copied");
+
+
+})
